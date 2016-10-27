e749d0cdf60c6dad0674542ba17ed3b11798e4b0
frantzke <lukas.frantzke@mail.utoronto.ca>
2016-10-27 19:34:30 -0400

    Add applicant and applicant by status

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -9,35 +9,52 @@ function selectStatus(){
     console.log(statusText);
 }
 
+function printList(taList){
+    let parent = $('#results');
+    for(let i = 0; i < taList.length; i++) {
+        let tmp = $('<li>').text(taList[i].givenname + '  ' + 
+                             taList[i].familyname + ' ' +
+                             taList[i].status + ' ' +
+                             taList[i].year);
+        parent.append(tmp);
+    }
+
+}
+
 // jQuery Documentpos
 $(document).ready(function() {
 
+    $("#RemoveByStunum").submit(function (e) {
+        e.preventDefault();
+        let num = $('#removeStunum').val();
+        $.ajax({
+            // The book id is part of the resource
+            url: '/applicants?stunum=' + num,
+            type: 'DELETE',
+            success: function result() {
+                location.reload(true);
+            }
+        });
+
+    });
+
     $("#AddAppl").submit(function (e) {
-        alert("@AddAppl");
-        alert($('form').serialize());
+        //alert("@AddAppl");
+        //alert($('form').serialize());
         e.preventDefault();
         console.log($('form').serialize());
         $.post('/addapplicant', $('form').serialize());
-        location.reload(true);
-
+        //location.reload(true);
     });
 
     $("#ApplByStatus").submit(function (e) {
         //TODO: Prevent adding more than once with multiple clicks
         //TODO sort by familyname
-        alert("@AddByStatus");
+        //alert("@AddByStatus");
         e.preventDefault();
         $.get("/applicants?status='"+statusText+"'", function(data){
-            let parent = $('#results');
             let taObj = JSON.parse(data);
-            let taArray = taObj.tas;
-            for(let i = 0; i < taArray.length; i++) {
-                let tmp = $('<li>').text(taArray[i].givenname + '  ' + 
-                                     taArray[i].familyname + ' ' +
-                                     taArray[i].status + ' ' +
-                                     taArray[i].year);
-                parent.append(tmp);
-            }
+            printList(taObj.tas);
         });
     });
 
@@ -46,36 +63,8 @@ $(document).ready(function() {
         //TODO sort by familyname
         e.preventDefault();
         $.get('/applicants', function(data){
-            let parent = $('#results');
             let taObj = JSON.parse(data);
-            let taArray = taObj.tas;
-            for(let i = 0; i < taArray.length; i++) {
-                let tmp = $('<li>').text(taArray[i].givenname + '  ' + 
-                                     taArray[i].familyname + ' ' +
-                                     taArray[i].status + ' ' +
-                                     taArray[i].year);
-                parent.append(tmp);
-            }
+            printList(taObj.tas);
         });
-        //location.reload(true);
-        //buildList();
     });
-
-function buildList() {
-    $.get('applicants', function (data) {
-        let parent = $('#results');
-        let taObj = JSON.parse(data);
-        let taArray = taObj.tas;
-        for(let i = 0; i < taArray.length; i++) {
-            let tmp = $('<li>').text(taArray[i].givenname + '  ' + 
-                                     taArray[i].familyname + ' ' +
-                                     taArray[i].status + ' ' +
-                                     taArray[i].year);
-                parent.append(tmp);
-        }
-    });
-}
-
-//buildList();
-
 });
